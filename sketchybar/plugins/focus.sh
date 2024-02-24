45982c54d617950c82bdd79397d65c39a98c845d
Panos Sakkos <panos.sakkos@gmail.com>
2024-02-24 19:44:56 +0200

    [sketchybar] format files

--- sketchybar/plugins/focus.sh
+++ sketchybar/plugins/focus.sh
@@ -3,7 +3,7 @@
 status=$(defaults read com.apple.controlcenter "NSStatusItem Visible FocusModes")
 
 if [ "$status" -eq 0 ]; then
-    sketchybar -m --set $NAME icon=""
+	sketchybar -m --set $NAME icon=""
 else
-    sketchybar -m --set $NAME icon=
+	sketchybar -m --set $NAME icon=
 fi
