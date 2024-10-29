061aff400d53bcf7f5c3fa93abe7da9b7a04870a
Panos Sakkos <panos.sakkos@gmail.com>
2024-10-29 21:36:15 +0200

    [sketchybar] format files

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -4,27 +4,27 @@ PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
 if [ $PERCENTAGE = "" ]; then
-	exit 0
+  exit 0
 fi
 
 case ${PERCENTAGE} in
 9[0-9] | 100)
-	ICON=""
-	;;
+  ICON=""
+  ;;
 [6-8][0-9])
-	ICON=""
-	;;
+  ICON=""
+  ;;
 [3-5][0-9])
-	ICON=""
-	;;
+  ICON=""
+  ;;
 [1-2][0-9])
-	ICON=""
-	;;
+  ICON=""
+  ;;
 *) ICON="" ;;
 esac
 
 if [[ $CHARGING != "" ]]; then
-	ICON=""
+  ICON=""
 
 fi
 
--- sketchybar/plugins/focus.sh
+++ sketchybar/plugins/focus.sh
@@ -3,7 +3,7 @@
 STATUS=$(defaults read com.apple.controlcenter "NSStatusItem Visible FocusModes")
 
 if [ "$STATUS" -eq 0 ]; then
-	sketchybar -m --set $NAME icon=""
+  sketchybar -m --set $NAME icon=""
 else
-	sketchybar -m --set $NAME icon=
+  sketchybar -m --set $NAME icon=
 fi
