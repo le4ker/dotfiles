33c08310e6a19f3d095c120e48dad28a17f0aee2
Panos Sakkos <panos.sakkos@gmail.com>
2024-12-04 15:48:01 +0200

    [sketchybar] update input

--- sketchybar/plugins/input.sh
+++ sketchybar/plugins/input.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 
 SOURCE=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleCurrentKeyboardLayoutInputSourceID)
-
 case ${SOURCE} in
-'com.apple.keylayout.ABC') ICON='' && LABEL='US' ;;
+'com.apple.keylayout.US') ICON='' && LABEL='US' ;;
 'com.apple.keylayout.Greek') ICON='󱌮' && LABEL='GR' ;;
 esac
 
